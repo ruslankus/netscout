12904d66cddeb4bf5a2b203f95ab0aeb82ba7240
Ruslan Kiricenko <ruslankus@yahoo.com>
2015-01-31 00:28:15 +0200

    minor fixes

--- protected/widgets/views/main_menu.php
+++ protected/widgets/views/main_menu.php
@@ -1,9 +1,9 @@
     <ul class="menu">
         <?php foreach($objMenu as $obj):?>
         <?php if(!empty($obj->route)):?>
-        <li><a href="<?echo $obj->route ?>"><?php echo $obj->label ?></a></li>
+        <li><a href="<?php echo $obj->route ?>"><?php echo $obj->label ?></a></li>
         <?php else:?>
-        <li><a href="/page/index/<?echo $obj->id?>"><?php echo $obj->label ?></a></li>
+        <li><a href="/page/index/<?php echo $obj->id ?>"><?php echo $obj->label ?></a></li>
         <?php endif;?>
         <?php endforeach;?>        
     </ul>

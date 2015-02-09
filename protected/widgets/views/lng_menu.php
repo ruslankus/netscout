9dbe63a2977b88f56d0bbb87b7f6f8934384648e
Ruslan Kiricenko <ruslankus@yahoo.com>
2015-02-09 18:47:08 +0200

    minor fix

--- protected/widgets/views/lng_menu.php
+++ protected/widgets/views/lng_menu.php
@@ -1,13 +1,22 @@
 
  <div class="lang-hlder">Lang:
  <?php $string = ''; foreach($objLngs as $lng):?>
-     <?php $params['language'] = $lng->lng_prefix;?> 
+     <?php
+      $params['language'] = $lng->lng_prefix;
+      if($current == $lng->lng_prefix){
+        $link = "<span>". strtoupper($lng->lng_prefix) ."</span>";   
+      }else{
+        $link = '<a href="'.Yii::app()->createUrl('/'.$controller.'/'.$action , $params).'">'.
+         strtoupper($lng->lng_prefix).'</a>';
+      }  
+        
+     
+     ?>
+      
     <?php if($objLngs[0] == $lng): ?>
-        <?php $string .=  '<a href="'.Yii::app()->createUrl('/'.$controller.'/'.$action , $params).'">'.
-         strtoupper($lng->lng_prefix).'</a>&nbsp;/' ?>
+        <?php $string .=  $link . '&nbsp;/' ?>
     <?php else:?>
-        <?php $string .=  '&nbsp;<a href="'.Yii::app()->createUrl('/'.$controller.'/'.$action , $params).'">'
-        . strtoupper($lng->lng_prefix).'</a>' ?>
+        <?php $string .=  '&nbsp;'.$link ?>
     <?php endif;?>
  <?php endforeach?> 
  <?php echo $string;  ?>
